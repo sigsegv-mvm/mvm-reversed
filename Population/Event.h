3f9935156e8e439699ca7bbe8759eb8b7d542ab8
sigsegv <sigsegv@sigpipe.info>
2015-10-25 21:03:59 -0700

    implement ParseEvent()

--- Population/Event.h
+++ Population/Event.h
@@ -13,5 +13,23 @@ struct Event
 
 Event *ParseEvent(KeyValues *kv)
 {
-	// TODO
+	Event *event = new Event();
+	
+	FOR_EACH_SUBKEY(kv, subkey) {
+		const char *name = subkey->GetName();
+		if (strlen(name) > 0) {
+			if (V_stricmp(name, "Target") == 0) {
+				event->target.sprintf(subkey->GetString(NULL));
+			} else if (V_stricmp(name, "Action") == 0) {
+				event->action.sprintf(subkey->GetString(NULL));
+			} else {
+				Warning("Unknown field '%s' in WaveSpawn event definition.\n",
+					subkey->GetString(NULL));
+				delete event;
+				return NULL;
+			}
+		}
+	}
+	
+	return event;
 }
