d325f3382d6621fb63bf90299d2ad735b8800a9f
sigsegv <sigsegv@sigpipe.info>
2015-10-28 15:55:28 -0700

    add some more partial class definitions

--- Misc.h
+++ Misc.h
@@ -62,3 +62,64 @@ private:
 	EventInfo m_OnBombDroppedOutput; // +0xc60
 	// TODO
 };
+
+
+enum TFNavAttributeType
+{
+	BLUE_SETUP_GATE             = (1 << 11),
+	RED_SETUP_GATE              = (1 << 12),
+	
+	BLOCKED_AFTER_POINT_CAPTURE = (1 << 13),
+	BLOCKED_UNTIL_POINT_CAPTURE = (1 << 14),
+	
+	BLUE_ONE_WAY_DOOR           = (1 << 15),
+	RED_ONE_WAY_DOOR            = (1 << 16),
+	
+	WITH_SECOND_POINT           = (1 << 17),
+	WITH_THIRD_POINT            = (1 << 18),
+	WITH_FOURTH_POINT           = (1 << 19),
+	WITH_FIFTH_POINT            = (1 << 20),
+	
+	SNIPER_SPOT                 = (1 << 21),
+	SENTRY_SPOT                 = (1 << 22),
+	
+	NO_SPAWNING                 = (1 << 25),
+	RESCUE_CLOSET               = (1 << 26),
+	
+	DOOR_NEVER_BLOCKS           = (1 << 28),
+	DOOR_ALWAYS_BLOCKS          = (1 << 29),
+	UNBLOCKABLE                 = (1 << 30),
+};
+
+
+class CTFNavArea : public CNavArea
+{
+public:
+	// TODO
+	
+private:
+	// TODO
+	TFNavAttributeType m_nAttributes; // +0x1c4
+	// TODO
+};
+
+
+class CTFGameRules : public CTeamplayRoundBasedRules
+{
+public:
+	// TODO
+	bool GameModeUsesUpgrades() { return this->m_bPlayingMannVsMachine; }
+	bool IsPvEModeActive()      { return this->m_bPlayingMannVsMachine; }
+	// TODO
+	bool IsPveModeControlled(CBaseEntity *ent)
+	{
+		return (ent != NULL && this->m_bPlayingMannVsMachine &&
+			ent->GetTeamNumber() == TF_TEAM_BLU);
+	}
+	// TODO
+	
+private:
+	// TODO
+	bool m_bPlayingMannVsMachine; // +0x96a
+	// TODO
+};
