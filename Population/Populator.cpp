e796f52f609325d9642d031b8a8fb702e6169f7f
sigsegv <sigsegv@sigpipe.info>
2015-11-06 03:28:08 -0800

    make significant progress on populator reverse engineering

--- Population/Populator.cpp
+++ Population/Populator.cpp
@@ -4,3 +4,308 @@
  */
 
 
+CRandomPlacementPopulator::CRandomPlacementPopulator(CPopulationManager *popmgr)
+	: m_PopMgr(popmgr)
+{
+	this->m_iCount = 0;
+	this->m_flMinimumSeparation = 0.0f;
+	// TODO
+}
+
+CPeriodicSpawnPopulator::CPeriodicSpawnPopulator(CPopulationManager *popmgr)
+	: m_PopMgr(popmgr)
+{
+	this->m_flMinInterval = 30.0f;
+	this->m_flMaxInterval = 30.0f;
+}
+
+CMissionPopulator::CMissionPopulator(CPopulationManager *popmgr)
+	: m_PopMgr(popmgr)
+{
+	// TODO
+	this->m_flInitialCooldown  = 0.0f;
+	this->m_flCooldownTime     = 0.0f;
+	this->m_iDesiredCount      = 0;
+	this->m_iBeginAtWave       = 0;
+	this->m_iEndBeforeThisWave = 9999;
+	// TODO
+}
+
+CWaveSpawnPopulator::CWaveSpawnPopulator(CPopulationManager *popmgr)
+	: m_PopMgr(popmgr)
+{
+	// TODO
+}
+
+CWave::CWave(CPopulationManager *popmgr)
+	: m_PopMgr(popmgr)
+{
+	// TODO
+}
+
+
+IPopulator::~IPopulator()
+{
+	if (this->m_Spawner != nullptr) {
+		delete this->m_Spawner;
+	}
+}
+
+CRandomPlacementPopulator::~CRandomPlacementPopulator()
+{
+}
+
+CPeriodicSpawnPopulator::~CPeriodicSpawnPopulator()
+{
+}
+
+CMissionPopulator::~CMissionPopulator()
+{
+}
+
+CWaveSpawnPopulator::~CWaveSpawnPopulator()
+{
+	// TODO
+}
+
+CWave::~CWave()
+{
+	// TODO
+}
+
+
+bool CRandomPlacementPopulator::Parse(KeyValues *kv)
+{
+	// TODO
+}
+
+bool CPeriodicSpawnPopulator::Parse(KeyValues *kv)
+{
+	// TODO
+}
+
+bool CMissionPopulator::Parse(KeyValues *kv)
+{
+	int runforthismanywaves = 99999;
+	
+	FOR_EACH_SUBKEY(kv, subkey) {
+		const char *name = subkey->GetName();
+		if (strlen(name) > 0) {
+			if (V_stricmp(name, "Objective") == 0) {
+				if (V_stricmp(subkey->GetString(nullptr), "DestroySentries") == 0 ||
+					V_stricmp(subkey->GetString(nullptr), "SeekAndDestroy") == 0) {
+					this->m_Objective = MISSION_DESTROY_SENTRIES;
+				} else if (V_stricmp(subkey->GetString(nullptr), "Sniper") == 0) {
+					this->m_Objective = MISSION_SNIPER;
+				} else if (V_stricmp(subkey->GetString(nullptr), "Spy") == 0) {
+					this->m_Objective = MISSION_SPY;
+				} else if (V_stricmp(subkey->GetString(nullptr), "Engineer") == 0) {
+					this->m_Objective = MISSION_ENGINEER;
+				} else {
+					Warning("Invalid mission '%s'\n", subkey->GetString(nullptr));
+					return false;
+				}
+			} else if (V_stricmp(name, "InitialCooldown") == 0) {
+				this->m_flInitialCooldown = subkey->GetFloat(nullptr);
+			} else if (V_stricmp(name, "CooldownTime") == 0) {
+				this->m_flCooldownTime = subkey->GetFloat(nullptr);
+			} else if (V_stricmp(name, "BeginAtWave") == 0) {
+				this->m_iBeginAtWave = subkey->GetInt(nullptr);
+			} else if (V_stricmp(name, "RunForThisManyWaves") == 0) {
+				runforthismanywaves = subkey->GetInt(nullptr);
+			} else if (V_stricmp(name, "DesiredCount") == 0) {
+				this->m_iDesiredCount = subkey->GetInt(nullptr);
+			} else {
+				/* BUG: duplicate spawner definitions will leak memory */
+				this->m_Spawner =
+					IPopulationSpawner::ParseSpawner(this, subkey);
+				if (this->m_Spawner == nullptr) {
+					Warning("Unknown attribute '%s' in Mission definition.\n",
+						name);
+				}
+			}
+		}
+	}
+	
+	this->m_iEndBeforeThisWave = this->m_iBeginAtWave + runforthismanywaves;
+	
+	return true;
+}
+
+bool CWaveSpawnPopulator::Parse(KeyValues *kv)
+{
+	// TODO
+}
+
+bool CWave::Parse(KeyValues *kv)
+{
+	// TODO
+}
+
+
+void IPopulator::PostInitialize(KeyValues *kv)
+{
+}
+
+void CRandomPlacementPopulator::PostInitialize(KeyValues *kv)
+{
+	// TODO
+}
+
+void CPeriodicSpawnPopulator::PostInitialize(KeyValues *kv)
+{
+	// TODO
+}
+
+
+void IPopulator::Update()
+{
+}
+
+void CPeriodicSpawnPopulator::Update()
+{
+	// TODO
+}
+
+void CMissionPopulator::Update()
+{
+	VPROF_BUDGET("CMissionPopulator::Update", "NextBot");
+	
+	
+	
+	// TODO
+}
+
+void CWaveSpawnPopulator::Update()
+{
+	// TODO
+}
+
+void CWave::Update()
+{
+	// TODO
+}
+
+
+void IPopulator::UnpauseSpawning()
+{
+}
+
+void CRandomPlacementPopulator::UnpauseSpawning()
+{
+	// TODO
+}
+
+void CPeriodicSpawnPopulator::UnpauseSpawning()
+{
+	// TODO
+}
+
+void CMissionPopulator::UnpauseSpawning()
+{
+	this->m_Timer1.Start(this->m_flCooldownTime);
+	this->m_Timer2.Start(RandomFloat(5.0f, 10.0f));
+}
+
+
+void IPopulator::OnPlayerKilled(CTFPlayer *player)
+{
+}
+
+void CWaveSpawnPopulator::OnPlayerKilled(CTFPlayer *player)
+{
+	// TODO
+}
+
+void CWave::OnPlayerKilled(CTFPlayer *player)
+{
+	// TODO
+}
+
+
+bool IPopulator::HasEventChangeAttributes(const char *name) const
+{
+	// TODO
+}
+
+bool CWave::HasEventChangeAttributes(const char *name) const
+{
+	// TODO
+}
+
+
+bool CMissionPopulator::UpdateMission(CTFBot::MissionType mtype)
+{
+	VPROF_BUDGET("CMissionPopulator::UpdateMission", "NextBot");
+	
+	// TODO
+}
+
+bool CMissionPopulator::UpdateMissionDestroySentries()
+{
+	VPROF_BUDGET("CMissionPopulator::UpdateMissionDestroySentries", "NextBot");
+	
+	// TODO
+}
+
+
+bool CWaveSpawnPopulator::IsFinishedSpawning()
+{
+	// TODO
+}
+
+bool CWave::IsDoneWithNonSupportWaves()
+{
+	// TODO
+}
+
+
+void CWaveSpawnPopulator::OnNonSupportWavesDone()
+{
+	// TODO
+}
+
+
+void CWaveSpawnPopulator::ForceFinish()
+{
+	// TODO
+}
+
+void CWave::ForceFinish()
+{
+	// TODO
+}
+
+
+void CWave::ForceReset()
+{
+	// TODO
+}
+
+
+void CWaveSpawnPopulator::SetState(CWaveSpawnPopulator::InternalStateType newstate)
+{
+	// TODO
+}
+
+
+int CWaveSpawnPopulator::GetCurrencyAmountPerDeath()
+{
+	// TODO
+}
+
+
+void CWave::ActiveWaveUpdate()
+{
+	// TODO
+}
+
+void CWave::WaveCompleteUpdate()
+{
+	// TODO
+}
+
+void CWave::WaveIntermissionUpdate()
+{
+	// TODO
+}
--- Population/Populator.h
+++ Population/Populator.h
@@ -7,14 +7,14 @@
 class IPopulator
 {
 public:
-	virtual ~IPopulator() {}
+	virtual ~IPopulator();
 	
-	virtual bool Parse(KeyValues *) = 0;
+	virtual bool Parse(KeyValues *kv) = 0;
 	virtual void PostInitialize();
 	virtual void Update();
 	virtual void UnpauseSpawning();
-	virtual void OnPlayerKilled(CTFPlayer *);
-	virtual bool HasEventChangeAttributes(const char *) const;
+	virtual void OnPlayerKilled(CTFPlayer *player);
+	virtual bool HasEventChangeAttributes(const char *name) const;
 	
 protected:
 	IPopulationSpawner *m_Spawner; // +0x04
@@ -25,92 +25,174 @@ protected:
 class CRandomPlacementPopulator : public IPopulator
 {
 public:
-	CRandomPlacementPopulator(CPopulationManager *);
+	CRandomPlacementPopulator(CPopulationManager *popmgr);
 	virtual ~CRandomPlacementPopulator();
 	
-	virtual bool Parse(KeyValues *);
+	virtual bool Parse(KeyValues *kv);
 	virtual void PostInitialize();
 	
 private:
-	// TODO: 0x0c+
+	int m_iCount;                // +0x0c
+	float m_flMinimumSeparation; // +0x10
+	// +0x14 NavAreaFilter dword -1 TODO: enum for this?
 };
 
 
 class CPeriodicSpawnPopulator : public IPopulator
 {
 public:
-	CPeriodicSpawnPopulator(CPopulationManager *);
+	CPeriodicSpawnPopulator(CPopulationManager *popmgr);
 	virtual ~CPeriodicSpawnPopulator();
 	
-	virtual bool Parse(KeyValues *);
+	virtual bool Parse(KeyValues *kv);
 	virtual void PostInitialize();
 	virtual void Update();
 	virtual void UnpauseSpawning();
 	
 private:
-	// TODO: 0x0c+
+	CSpawnLocation m_Where; // +0x0c
+	float m_flMinInterval;  // +0x24
+	float m_flMaxInterval;  // +0x28
+	// +0x2c CountdownTimer
 };
 
 
 class CMissionPopulator : public IPopulator
 {
 public:
-	CMissionPopulator(CPopulationManager *);
+	CMissionPopulator(CPopulationManager *popmgr);
 	virtual ~CMissionPopulator();
 	
-	virtual bool Parse(KeyValues *);
+	virtual bool Parse(KeyValues *kv);
 	virtual void Update();
 	virtual void UnpauseSpawning();
 	
-	// UpdateMission(CTFBot::MissionType)
-	// UpdateMissionDestroySentries()
+	bool UpdateMission(CTFBot::MissionType mtype);
+	bool UpdateMissionDestroySentries();
 	
 private:
-	// TODO: 0x0c+
+	CTFBot::MissionType m_Objective; // +0x0c
+	CSpawnLocation m_Where;          // +0x10
+	// +0x28 dword 0
+	float m_flInitialCooldown;       // +0x2c
+	float m_flCooldownTime;          // +0x30
+	CountdownTimer m_Timer1;         // +0x34
+	CountdownTimer m_Timer2;         // +0x40
+	int m_iDesiredCount;             // +0x4c
+	int m_iBeginAtWave;              // +0x50
+	int m_iEndBeforeThisWave;        // +0x54
 };
 
 
 class CWaveSpawnPopulator : public IPopulator
 {
 public:
-	CWaveSpawnPopulator(CPopulationManager *);
+	enum InternalStateType
+	{
+		// TODO
+	};
+	
+	CWaveSpawnPopulator(CPopulationManager *popmgr);
 	virtual ~CWaveSpawnPopulator();
 	
-	virtual bool Parse(KeyValues *);
+	virtual bool Parse(KeyValues *kv);
 	virtual void Update();
-	virtual void OnPlayerKilled(CTFPlayer *);
+	virtual void OnPlayerKilled(CTFPlayer *player);
 	
-	// ForceFinish()
-	// GetCurrencyAmountPerDeath()
-	// IsFinishedSpawning()
-	// OnNonSupportWavesDone()
-	// SetState(CWaveSpawnPopulator::InternalStateType)
+	bool IsFinishedSpawning();
+	void OnNonSupportWavesDone();
+	void ForceFinish();
+	
+	void SetState(CWaveSpawnPopulator::InternalStateType newstate);
+	
+	int GetCurrencyAmountPerDeath();
 	
 private:
-	// TODO: 0x0c+
+	static int m_reservedPlayerSlotCount = ???;
+	
+	// +0x00c CSpawnLocation
+	// +0x024 TotalCount dword 0
+	// +0x028 
+	// +0x02c 
+	// +0x030 MaxActive dword decimal 999
+	// +0x034 SpawnCount dword 1
+	// +0x038 WaitBeforeStarting float 0
+	// +0x03c float 0
+	// +0x040 byte 0
+	// +0x044 StartWaveWarningSound CFmtStrN<256>
+	// +0x150 StartWaveOutput EventInfo *
+	// +0x154 FirstSpawnWarningSound CFmtStrN<256>
+	// +0x260 FirstSpawnOutput EventInfo *
+	// +0x264 LastSpawnWarningSound CFmtStrN<256>
+	// +0x370 LastSpawnOutput EventInfo *
+	// +0x374 DoneWarningSound CFmtStrN<256>
+	// +0x480 DoneOutput EventInfo *
+	// +0x484 TotalCurrency dword
+	// +0x488 
+	// +0x48c Name CUtlString
+	// +0x490 WaitForAllSpawned CUtlString
+	// +0x494 dword/float 0
+	// +0x498 CountdownTimer
+	// +0x4a4 dword/float 0
+	// +0x4a8 dword/float 0
+	// +0x4ac dword/float 0
+	// +0x4b0 dword/float 0
+	// +0x4b4 dword/float 0
+	// +0x4b8 
+	// +0x4bc 
+	// +0x4c0 Support bool
+	// +0x4c1 SupportLimited bool
+	// +0x4c4 dword/float 0
+	// +0x4c8 
+	// +0x4cc RandomSpawn bool
+	// +0x4d0 dword/float 0
 };
 
 
 class CWave : public IPopulator
 {
 public:
-	CWave(CPopulationManager *);
+	CWave(CPopulationManager *popmgr);
 	virtual ~CWave();
 	
-	virtual bool Parse(KeyValues *);
+	virtual bool Parse(KeyValues *kv);
 	virtual void Update();
-	virtual void OnPlayerKilled(CTFPlayer *);
-	virtual bool HasEventChangeAttributes(const char *) const;
+	virtual void OnPlayerKilled(CTFPlayer *player);
+	virtual bool HasEventChangeAttributes(const char *name) const;
 	
-	// ActiveWaveUpdate()
-	// AddClassType(string_t, int, uint)
-	// FindWaveSpawnPopulator(char const*)
-	// ForceFinish()
-	// ForceReset()
-	// IsDoneWithNonSupportWaves()
-	// WaveCompleteUpdate()
-	// WaveIntermissionUpdate()
+	bool IsDoneWithNonSupportWaves();
+	void ForceFinish();
+	void ForceReset();
+	
+	void ActiveWaveUpdate();
+	void WaveCompleteUpdate();
+	void WaveIntermissionUpdate();
+	
+	CWaveSpawnPopulator *FindWaveSpawnPopulator(char const *name);
+	void AddClassType(string_t icon, int count, unsigned int flags);
 	
 private:
-	// TODO: 0x0c+
+	CUtlVectorAutoPurge<CWaveSpawnPopulator *> m_WaveSpawns;
+	// +0x020 byte 0
+	// +0x021 byte 0
+	// +0x024 
+	// +0x028 
+	// +0x02c 
+	// +0x030 
+	// +0x034 
+	// +0x038 CUtlVector<WaveClassCount_t> (aka int)
+	// +0x04c 
+	// +0x050 StartWaveOutput EventInfo *
+	// +0x054 DoneOutput EventInfo *
+	// +0x058 InitWaveOutput EventInfo *
+	// +0x05c Description CFmtStr<256>
+	// +0x168 Sound CFmtStr<256>
+	// +0x274 WaitWhenDone float
+	// +0x278 CountdownTimer
+	// +0x284 byte 1
+	// +0x288 
+	// +0x28c byte 0
+	// +0x290 CountdownTimer
+	// +0x29c byte 0
+	// +0x2a0 
 };
