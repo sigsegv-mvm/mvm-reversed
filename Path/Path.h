81bfbf09ab80f45b94aa9e22a03bfa21d7e262a9
sigsegv <sigsegv@sigpipe.info>
2015-11-15 02:16:51 -0800

    add missing return type placeholders for class Path

--- Path/Path.h
+++ Path/Path.h
@@ -30,42 +30,42 @@ public:
 	Path();
 	virtual ~Path();
 	
-	virtual GetLength() const;
+	virtual UNKNOWN GetLength() const;
 	
-	virtual GetPosition(float f1, const Segment *seg) const;
-	virtual GetClosestPosition(const Vector& v1, const Segment *seg, float f1) const;
-	virtual GetStartPosition() const;
-	virtual GetEndPosition() const;
+	virtual UNKNOWN GetPosition(float f1, const Segment *seg) const;
+	virtual UNKNOWN GetClosestPosition(const Vector& v1, const Segment *seg, float f1) const;
+	virtual UNKNOWN GetStartPosition() const;
+	virtual UNKNOWN GetEndPosition() const;
 	
-	virtual GetSubject() const;
-	virtual GetCurrentGoal() const;
-	virtual GetAge() const;
+	virtual UNKNOWN GetSubject() const;
+	virtual UNKNOWN GetCurrentGoal() const;
+	virtual UNKNOWN GetAge() const;
 	
-	virtual MoveCursorToClosestPosition(const Vector&, SeekType stype, float f1) const;
-	virtual MoveCursorToStart();
-	virtual MoveCursorToEnd();
-	virtual MoveCursor(float f1, MoveCursorType mctype);
+	virtual UNKNOWN MoveCursorToClosestPosition(const Vector&, SeekType stype, float f1) const;
+	virtual UNKNOWN MoveCursorToStart();
+	virtual UNKNOWN MoveCursorToEnd();
+	virtual UNKNOWN MoveCursor(float f1, MoveCursorType mctype);
 	
-	virtual GetCursorPosition() const;
-	virtual GetCursorData() const;
+	virtual UNKNOWN GetCursorPosition() const;
+	virtual UNKNOWN GetCursorData() const;
 	
-	virtual IsValid() const;
-	virtual Invalidate();
+	virtual UNKNOWN IsValid() const;
+	virtual UNKNOWN Invalidate();
 	
-	virtual Draw(const Segment *seg) const;
-	virtual DrawInterpolated(float f1, float f2);
+	virtual UNKNOWN Draw(const Segment *seg) const;
+	virtual UNKNOWN DrawInterpolated(float f1, float f2);
 	
-	virtual FirstSegment() const;
-	virtual NextSegment(const Segment *seg) const;
-	virtual PriorSegment(const Segment *seg) const;
-	virtual LastSegment() const;
+	virtual UNKNOWN FirstSegment() const;
+	virtual UNKNOWN NextSegment(const Segment *seg) const;
+	virtual UNKNOWN PriorSegment(const Segment *seg) const;
+	virtual UNKNOWN LastSegment() const;
 	
-	virtual OnPathChanged(INextBot *nextbot, ResultType rtype);
+	virtual UNKNOWN OnPathChanged(INextBot *nextbot, ResultType rtype);
 	
-	virtual Copy(INextBot *nextbot, const Path& that);
+	virtual UNKNOWN Copy(INextBot *nextbot, const Path& that);
 	
-	virtual ComputeWithOpenGoal(INextBot *nextbot, const IPathCost& cost, const IPathOpenGoalSelector& sel, float f1);
-	virtual ComputeAreaCrossing(INextBot *nextbot, const CNavArea *area1, const Vector& v1, const CNavArea *area2, NavDirType ndtype, Vector *v2) const;
+	virtual UNKNOWN ComputeWithOpenGoal(INextBot *nextbot, const IPathCost& cost, const IPathOpenGoalSelector& sel, float f1);
+	virtual UNKNOWN ComputeAreaCrossing(INextBot *nextbot, const CNavArea *area1, const Vector& v1, const CNavArea *area2, NavDirType ndtype, Vector *v2) const;
 	
 	UNKNOWN AssemblePrecomputedPath(INextBot *nextbot, const Vector& v1, CNavArea *area);
 	UNKNOWN BuildTrivialPath(INextBot *nextbot, const Vector& v1);
