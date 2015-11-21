3c8cadeae58c2fe95533d0476edd5047d6408afb
sigsegv <sigsegv@sigpipe.info>
2015-11-20 20:20:33 -0800

    get return types for class ChasePath functions

--- Path/ChasePath.h
+++ Path/ChasePath.h
@@ -7,19 +7,19 @@
 class ChasePath : public PathFollower
 {
 public:
-	ChasePath(/* TODO */);
+	ChasePath();
 	virtual ~ChasePath();
 	
-	virtual UNKNOWN Invalidate() override;
+	virtual void Invalidate() override;
 	
-	virtual UNKNOWN Update(INextBot *nextbot, CBaseEntity *ent, const IPathCost& cost, Vector *v1);
-	virtual UNKNOWN GetLeadRadius() const;
-	virtual UNKNOWN GetMaxPathLength() const;
-	virtual UNKNOWN PredictSubjectPosition(INextBot *nextbot, CBaseEntity *ent) const;
-	virtual UNKNOWN IsRepathNeeded(INextBot *nextbot, CBaseEntity *ent) const;
-	virtual UNKNOWN GetLifetime() const;
+	virtual void Update(INextBot *nextbot, CBaseEntity *ent, const IPathCost& cost_func, Vector *v1);
+	virtual float GetLeadRadius() const;
+	virtual float GetMaxPathLength() const;
+	virtual Vector PredictSubjectPosition(INextBot *nextbot, CBaseEntity *ent) const;
+	virtual bool IsRepathNeeded(INextBot *nextbot, CBaseEntity *ent) const;
+	virtual float GetLifetime() const;
 	
-	UNKNOWN RefreshPath(INextBot *nextbot, CBaseEntity *ent, const IPathCost& cost, Vector *v1);
+	void RefreshPath(INextBot *nextbot, CBaseEntity *ent, const IPathCost& cost_func, Vector *v1);
 	
 protected:
 	CountdownTimer m_ctTimer1; // +0x47d4
