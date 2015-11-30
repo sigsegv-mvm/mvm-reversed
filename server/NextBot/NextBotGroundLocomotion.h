6fe65a505a3a830f9528eaa2d3c40089cfc2b417
sigsegv <sigsegv@sigpipe.info>
2015-11-29 21:32:13 -0800

    get member information for class NextBotGroundLocomotion

--- server/NextBot/NextBotGroundLocomotion.h
+++ server/NextBot/NextBotGroundLocomotion.h
@@ -4,8 +4,24 @@
  */
 
 
+// sizeof: 0x13c
 class NextBotGroundLocomotion : public ILocomotion
 {
 public:
 	// TODO
+	
+private:
+	// 058 dword 0
+	// ...
+	// 0b0 dword -1
+	// ...
+	// 0d4 dword 0
+	// ...
+	// 0e0 CountdownTimer
+	// 0ec CountdownTimer
+	// 0f8 
+	// ...
+	// 128 byte 0
+	// 12c CountdownTimer
+	// 138 dword -1
 };
