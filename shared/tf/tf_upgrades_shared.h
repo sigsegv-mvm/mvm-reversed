1686f3cef0f7d3fdc902692fc201612fdc0d441a
sigsegv <sigsegv@sigpipe.info>
2015-12-21 20:56:15 -0800

    get members for struct CMannVsMachineUpgrades

--- shared/tf/tf_upgrades_shared.h
+++ shared/tf/tf_upgrades_shared.h
@@ -7,7 +7,14 @@
 // sizeof: 0x15c
 struct CMannVsMachineUpgrades
 {
-	// TODO
+	char m_szAttribute[0x40]; // +0x000
+	char m_szIcon[MAX_PATH];  // +0x040
+	float m_flIncrement;      // +0x144
+	float m_flCap;            // +0x148
+	int m_nCost;              // +0x14c
+	int m_iUIGroup;           // +0x150
+	int m_iQuality;           // +0x154
+	int m_iTier;              // +0x158
 };
 
 
