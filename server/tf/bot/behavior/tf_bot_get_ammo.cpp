c974772c6209a34fd952fbcf6658542130f42bd2
sigsegv <sigsegv@sigpipe.info>
2016-04-20 13:52:59 -0700

    implement parts of CTFBotGetAmmo and CTFBotGetHealth

--- server/tf/bot/behavior/tf_bot_get_ammo.cpp
+++ server/tf/bot/behavior/tf_bot_get_ammo.cpp
@@ -13,7 +13,68 @@ ConVar tf_bot_ammo_search_range("tf_bot_ammo_search_range", "5000", FCVAR_CHEAT,
 ConVar tf_bot_debug_ammo_scavanging("tf_bot_debug_ammo_scavanging", "0", FCVAR_CHEAT);
 
 
+CTFBotGetAmmo::CTFBotGetAmmo(/* TODO */)
+{
+	// TODO
+}
 
+CTFBotGetAmmo::~CTFBotGetAmmo()
+{
+}
+
+
+const char *CTFBotGetAmmo::GetName() const
+{
+	return "GetAmmo";
+}
+
+
+ActionResult<CTFBot> CTFBotGetAmmo::OnStart(CTFBot *actor, Action<CTFBot> *action)
+{
+	VPROF_BUDGET("CTFBotGetAmmo::OnStart", "NextBot");
+	
+	// TODO
+}
+
+ActionResult<CTFBot> CTFBotGetAmmo::Update(CTFBot *actor, float dt)
+{
+	// TODO
+}
+
+
+EventDesiredResult<CTFBot> CTFBotGetAmmo::OnContact(CTFBot *actor, CBaseEntity *ent, CGameTrace *trace)
+{
+	return EventDesiredResult<CTFBot>::Continue();
+}
+
+EventDesiredResult<CTFBot> CTFBotGetAmmo::OnMoveToSuccess(CTFBot *actor, const Path *path)
+{
+	return EventDesiredResult<CTFBot>::Continue();
+}
+
+EventDesiredResult<CTFBot> CTFBotGetAmmo::OnMoveToFailure(CTFBot *actor, const Path *path, MoveToFailureType fail)
+{
+	return EventDesiredResult<CTFBot>::Done("Failed to reach ammo", ResultSeverity::CRITICAL);
+}
+
+EventDesiredResult<CTFBot> CTFBotGetAmmo::OnStuck(CTFBot *actor)
+{
+	return EventDesiredResult<CTFBot>::Done("Stuck trying to reach ammo", ResultSeverity::CRITICAL);
+}
+
+
+QueryResponse CTFBotGetAmmo::ShouldHurry(const INextBot *nextbot) const
+{
+	return QueryResponse::YES;
+}
+
+
+bool CTFBotGetAmmo::IsPossible(CTFBot *actor)
+{
+	VPROF_BUDGET("CTFBotGetAmmo::IsPossible", "NextBot");
+	
+	// TODO
+}
 
 
 CAmmoFilter::CAmmoFilter(CBaseEntity *actor)
--- server/tf/bot/behavior/tf_bot_get_health.cpp
+++ server/tf/bot/behavior/tf_bot_get_health.cpp
@@ -14,7 +14,63 @@ ConVar tf_bot_health_search_near_range("tf_bot_health_search_near_range", "1000"
 ConVar tf_bot_health_search_far_range("tf_bot_health_search_far_range", "2000", FCVAR_CHEAT);
 
 
+CTFBotGetHealth::CTFBotGetHealth(/* TODO */)
+{
+	// TODO
+}
 
+CTFBotGetHealth::~CTFBotGetHealth()
+{
+}
+
+
+const char *CTFBotGetHealth::GetName() const
+{
+	return "GetHealth";
+}
+
+
+ActionResult<CTFBot> CTFBotGetHealth::OnStart(CTFBot *actor, Action<CTFBot> *action)
+{
+	VPROF_BUDGET("CTFBotGetHealth::OnStart", "NextBot");
+	
+	// TODO
+}
+
+ActionResult<CTFBot> CTFBotGetHealth::Update(CTFBot *actor, float dt)
+{
+	// TODO
+}
+
+
+EventDesiredResult<CTFBot> CTFBotGetHealth::OnMoveToSuccess(CTFBot *actor, const Path *path)
+{
+	return EventDesiredResult<CTFBot>::Continue();
+}
+
+EventDesiredResult<CTFBot> CTFBotGetHealth::OnMoveToFailure(CTFBot *actor, const Path *path, MoveToFailureType fail)
+{
+	return EventDesiredResult<CTFBot>::Done("Failed to reach health kit", ResultSeverity::CRITICAL);
+}
+
+EventDesiredResult<CTFBot> CTFBotGetHealth::OnStuck(CTFBot *actor)
+{
+	return EventDesiredResult<CTFBot>::Done("Stuck trying to reach health kit", ResultSeverity::CRITICAL);
+}
+
+
+QueryResponse CTFBotGetHealth::ShouldHurry(const INextBot *nextbot) const
+{
+	return QueryResponse::YES;
+}
+
+
+bool CTFBotGetHealth::IsPossible(CTFBot *actor)
+{
+	VPROF_BUDGET("CTFBotGetHealth::IsPossible", "NextBot");
+	
+	// TODO
+}
 
 
 CHealthFilter::CHealthFilter(CBaseEntity *actor)
